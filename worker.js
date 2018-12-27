87db10acf7222ffd6f3f8db02ba1b3a350c85e3d
Shiang-Yu Huang <sky90456@hotmail.com>
2018-12-27 22:06:55 +0800

    fix procfile worng

--- worker.js
+++ worker.js
@@ -4,6 +4,8 @@ const CrawlerScript = require('./scripts/CrawlerScript')
 const Notify = require('./scripts/NotifyScript')
 
 
+
+
 //script
 const crawlerScript = new CrawlerScript()
 const notify = new Notify()
