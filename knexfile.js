1df6d8326e3af7a230ebffa936af75802f04da69
Shiang-Yu Huang <sky90456@hotmail.com>
2018-12-29 20:35:34 +0800

    change db to nas docker

--- knexfile.js
+++ knexfile.js
@@ -19,9 +19,9 @@ module.exports = {
       user: process.env.PG_USER,
       password: process.env.PG_PASSWORD,
       database: process.env.PG_DATABASE,
-      ssl: true
-    },
-    pool: { min: 0, max: 5 }
+      port: process.env.PG_PORT,
+      // ssl: true
+    }
   }
 
 };
